af14c0980582aee8a1b26e3c6163dce4012381cc
Tolu Adebayo <74066833+SangChii@users.noreply.github.com>
2020-11-19 18:37:13 -0500

    update to text

--- app/text_gen.py
+++ app/text_gen.py
@@ -1,11 +1,35 @@
+# create a file with the name random.txt of with 100 random characters
+import random 
+import string 
+import os 
+import os.path
+from os import path
 
-from flask import Flask 
-app=Flask(__name__)
-@app.route('/')
-def hello ():
-	return "Hello Worl!d"
+def findingFiles(): #function that finds files
+    fileExistence = print("File exists:"+str(path.exists("random.txt"))) #prints a boolean if the file exists 
+    if fileExistence == True: #if the file exists 
+        file = open("random.txt", "w") #open the file 
+        #print out existing content
+        print (file.read())
+        #then re-write the file with new random text data
+        
+        #and print out the new test data.
+    else:
+        print("No such file exists")
+        
+def randomCharacters(length):
+    text = " " #start off with an empty string 
+    fileName = open("random.txt", "w")
+    for i in range(length):
+        text += random.choice(string.ascii_letters + string.digits + string.hexdigits)#adding ascii and string sto make up the characters for the text 
+    fileName.write(text)
+    fileName.close()
+    #open the file 
+    fileOpen = open("random.txt")
+    
 
-if __name__== "__main__":
-	app.run(host= '0.0.0.0') 
-
+findingFiles()
+randomCharacters(100)
+findingFiles()
+randomCharacters(100)
 
